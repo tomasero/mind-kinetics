b543c1af562e2752eb5531d30f82466e035b14ae
jonferran <jonferran@yahoo.com>
2015-01-27 23:31:36 -0800

    Comment Massage
    Removed example code comments

--- arm/jonExploBot/jonExploBot.ino
+++ arm/jonExploBot/jonExploBot.ino
@@ -1,11 +1,4 @@
-// Bounce.pde
-// -*- mode: C++ -*-
-//
-// Make a single stepper bounce from one limit to another
-//
-// Copyright (C) 2012 Mike McCauley
-// $Id: Random.pde,v 1.1 2011/01/05 01:51:01 mikem Exp mikem $
-
+// 
 #include <AccelStepper.h>
 #include <Servo.h>
 //#include <Serial.h>
